aa8327fb637032ddbd90b915cd41d2318f2785e4
daya444 <daya@444>
2024-08-28 00:16:49 +0530

    recent

---
+++ src/Eco-system.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export const Ecosystem = () => {
+  return (
+    <div >
+        <div className='bg-[#005585] p-4' >
+             <div className='text-base font-bold text-white'>
+              
+               GenAI in Life Insurance Fraud Detection: Revolutionizing Industry Standards
+               
+             </div>
+
+             <div className='text-sm text-white'>
+               Employing advanced algorithms and data analysis, life insurance firms are leveraging GenAI to detect fraud more effectively, improve claim accuracy, and ensure policyholder trust, thereby setting new benchmarks in industry practices.
+             </div>
+
+        </div>
+
+            
+
+
+        <div className='mx-3 my-8 '>
+            
+            <div className=''>
+                Lorem ipsum dolor 
+            </div>
+
+
+        </div>
+
+    </div>
+  )
+}
+
+
+
